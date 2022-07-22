15a9e286beaf68ca3e50d88a38ae792e5f51ab00
Purodhika <purodhika.s@gmail.com>
2022-07-22 02:08:55 -0400

    MongoDB integration done

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //"URI": "mongodb://localhost/business"
+  "URI":  "mongodb+srv://Purodhika:purodhika@surveymakers.nc7dttc.mongodb.net/Surveys?retryWrites=true&w=majority"
+}
